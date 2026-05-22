985cdb027f87f59b91306d0943f0c39854169e96
celesteFernandez <celestefernandezmansilla@gmail.com>
2026-05-22 00:01:03 -0300

    se agrega controller y route para user

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const {sequelize} = require('./db/models')
-const dotenv = require('dotenv');
-dotenv.config()
-const PORT = process.env.PORT || 3001
+const { sequelize } = require("./db/models");
+const { User } = require("./db/models");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+const usersRouter = require("./routes/user.routes");
+app.use(express.json());
+app.use("/users", usersRouter);
 
-app.use(express.json())
-
-app.listen(PORT, async (err)=> {
-    if(err) {
-        console.error(err.message)
-        process.exit(1)
-    }
-    await sequelize.sync({force: true})
-    console.log(`App iniciada en el puerto ${PORT}`)
-})
+app.listen(PORT, async (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+  await sequelize.sync({ force: true });
+  console.log(`App iniciada en el puerto ${PORT}`);
+});
