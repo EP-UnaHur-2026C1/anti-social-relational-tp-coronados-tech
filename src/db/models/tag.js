d9ae7bc96a9729454086139d2f6c2a5215c432c2
RafaelBarberiS <rafaelbarberis@gmail.com>
2026-05-24 21:32:43 -0300

    Fix: correcion en la relacion muchos a muchos entre los modelos POST y TAG

--- src/db/models/tag.js
+++ src/db/models/tag.js
@@ -1,26 +1,39 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Tag extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            Tag.hasMany(models.Post, { foreignKey: "post_id", as: "posts" });
-        }
+  class Tag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Tag.belongsToMany(models.Post, {
+        through: "Post_Tag",
+        foreignKey: "tag_id",
+        otherKey: "post_id",
+        as: "posts",
+      });
     }
-    Tag.init(
-        {
-            name: { type: DataTypes.STRING, allowNull: false },
-            createdAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-            updatedAt: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
-        },
-        {
-            sequelize,
-            modelName: "Tag",
-        },
-    );
-    return Tag;
+  }
+  Tag.init(
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Tag",
+    },
+  );
+  return Tag;
 };
