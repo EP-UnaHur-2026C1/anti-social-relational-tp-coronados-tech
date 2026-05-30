01c785f9dc824890e70526cbf0955e4bceca30c8
celesteFernandez <celestefernandezmansilla@gmail.com>
2026-05-30 14:08:58 -0300

    se agrega error por duplicacion en user y su traudccion

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -12,6 +12,15 @@ const errorMiddleware = (err, req, res, next) => {
     });
   }
 
+  if (err.name === 'SequelizeUniqueConstraintError') {
+    const field = err.errors[0].path; 
+    
+    return res.status(HTTP.CONFLICT || 409).json({
+      message: res.__("user_already_exists", { field }), 
+      error: err.message
+    });
+  }
+
   console.error(err);
   res.status(HTTP.INTERNAL_SERVER_ERROR).json({
     message: res.__("error_internal"),
