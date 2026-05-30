abf9ea85a7a9792d8e04dceac82bb481a5ad0226
CelesteFernandez <94650466+CelesteFernandez@users.noreply.github.com>
2026-05-30 20:06:30 -0300

    16 - schema user (#18)
    * elimino try/catch y agrego el uso de las constantes
    
    * user schema added
    
    * deleted middleware for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,69 +1,41 @@
+const HTTP = require("../config/HttpCode");
 const { User } = require('../db/models');
 
 // POST/users - crea un nuevo usuario
 const createUser = async (req, res) =>{
-    try {
-        const data = req.body;
-        const user = await User.create(data);
-        res.status(201).json(user);
-    } catch (error){
-        res.status(500).json({ message: "Error al crear el usuario:", error: error.message });
-    }
+    const data = req.body;
+    const user = await User.create(data);
+    res.status(HTTP.CREATED).json(user);
 }
 
 //GET/users - obtiene todos los usuarios
 const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.findAll();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({ message: "Error al obtener los usuarios:", error: error.message });
-    }
+    const users = await User.findAll();
+    res.status(HTTP.OK).json(users);
 }
 
 //GET/users/:id - obtiene un usuario por su ID
 const getUserById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ message: "Usuario no encontrado" });
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: "Error al obtener el usuario:", error: error.message });
-    }
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    res.status(HTTP.OK).json(user);
 }
 
 //PUT/users/:id - actualiza un usuario por su ID
 const updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const data = req.body;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ message: "Usuario no encontrado" });
-        }
-        await user.update(data);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({ message: "Error al actualizar el usuario:", error: error.message });
-    }
+    const { id } = req.params;
+    const data = req.body;
+    const user = await User.findByPk(id);
+    await user.update(data);
+    res.status(HTTP.OK).json(user);
 }
 
 //DELETE/users/:id - elimina un usuario por su ID
 const deleteUser = async (req, res) => {
     const { id } = req.params;
-    try {
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ message: "Usuario no encontrado" });
-        }
-        await user.destroy();
-        res.status(200).json({ message: "Usuario eliminado exitosamente" });
-    } catch (error) {
-        res.status(500).json({ message: "Error al eliminar el usuario:", error: error.message });
-    }
+    const user = await User.findByPk(id);
+    await user.destroy();
+    res.status(HTTP.OK).json({ message: "Usuario eliminado exitosamente" });
 }
 
 
