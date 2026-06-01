2c18edb069017259bf4fb299871eb760f41f5063
RafaelBarberiS <rafaelbarberis@gmail.com>
2026-06-01 15:26:28 -0300

    fix: control cache de post en tags

--- src/services/tag.service.js
+++ src/services/tag.service.js
@@ -1,4 +1,5 @@
 const { Tag, Post } = require("../db/models");
+const postCache = require("./postCache.service");
 
 const tagIncludes = [
   {
@@ -10,12 +11,12 @@ const tagIncludes = [
 ];
 
 const findAll = ({ post_id } = {}) => {
-  const postsInclude = {
+  /*const postsInclude = {
     model: Post,
     as: "posts",
     attributes: ["id", "description"],
     through: { attributes: [] },
-  };
+  };*/
 
   if (post_id !== undefined) {
     postsInclude.where = { id: post_id };
@@ -34,6 +35,7 @@ const create = async ({ name, post_id }) => {
     const post = await Post.findByPk(post_id);
     if (post) {
       await post.addTag(tag);
+      postCache.deletePost(post_id);
     }
   }
 
@@ -47,6 +49,7 @@ const update = async (id, { name }) => {
   if (name === undefined) return { empty: true };
 
   await tag.update({ name });
+  postCache.deletePost(tag.post_id);
 
   return Tag.findByPk(id, { include: tagIncludes });
 };
@@ -56,6 +59,7 @@ const assignToPost = async (id, post_id) => {
   const tag = await Tag.findByPk(id);
 
   await post.addTag(tag);
+  postCache.deletePost(post_id);
 
   return Tag.findByPk(id, { include: tagIncludes });
 };
@@ -65,8 +69,9 @@ const remove = async (id) => {
 
   if (!tag) return false;
 
+  const { post_id } = tag;
   await tag.destroy();
-
+  postCache.deletePost(post_id);
   return true;
 };
 
